386bd34c06f1787047c62023a30f69bf0f8f9a6e
sunShineLoveMe <juneandrao@outlook.com>
2023-05-05 14:33:46 +0800

    push component like Lottery

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,10 @@
-import { ConnectButton } from "web3uikit"
+import { ConnectButton } from "@web3uikit/web3"
 
 export default function Header() {
     return (
         <div>
+            Decentralized Lottory
+            {/* 在 React Moralis 中， 表示禁用 Moralis 的身份验证功能，以便快速连接用户的钱包，并实现 Web3 操作 */}
             <ConnectButton moralisAuth={false} />
         </div>
     )
---
+++ components/LotteryEntrance.js
@@ -0,0 +1,5 @@
+export default function LotteryEntrance() {
+    return (
+        <div>Hi from lottery entrance!</div>        
+    )
+}
